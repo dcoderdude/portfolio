df6b39c357ac7eb8bf98c503549bcb6f73a8c016
Dan H <daniel.hernandez.dev@gmail.com>
2021-12-10 23:47:57 -0800

    add contact direct message link to the about section.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import About from './components/About';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <main className="App">
       <About />
+      <Navbar />
+      <Skills />
+      <Contact />
     </main>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,21 @@
 import styles from '../../src/modules/about.module.css'
-import resume from '../../src/resume.pdf';
 
 export default function About() 
 {
     return (
-        <div className={styles.container}>
-            <h1>
-                <div>Hi, I'm Dan</div>
-                <br />Full-stack & Web developer.
-            </h1>
-            <button>
-                <a href={resume} download="DanHernandez.pdf">Download Resume</a>
-            </button>
-        </div>
+        <section id ="about">
+            <div className={styles.outer_container}>
+                <div className={styles.inner_container}>
+                    <h1 className={styles.header}>Hi, I'm Dan
+                        <br/> Full Stack Web Developer
+                    </h1>
+                    <div className={styles.contact_info}>
+                        <a href="#contact">
+                            Quick Response
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
