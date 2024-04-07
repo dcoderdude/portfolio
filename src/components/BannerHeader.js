ab64e8718d33d766c2c86d09e0185026a9f458b3
dan7791-dev <daniel.hernandez.dev@gmail.com>
2024-04-07 16:10:41 -0700

    refactor content size and location on landing page.

--- src/components/BannerHeader.js
+++ src/components/BannerHeader.js
@@ -2,19 +2,30 @@ import { BsFillMoonStarsFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 export default function BannerHeader({ darkMode: darkModeState, updateDarkModeState }) {
+    const cv = 'CV.pdf';
     const handleChange = () => {
         const newValue = darkModeState === 'dark' ? 'light' : 'dark';
         updateDarkModeState(newValue);
     }
-    const cv = 'CV.pdf';
 
     return (
         <section>
             <nav className='flex p-5 justify-center'>
-                <ul className='flex py-5 items-center'>
-                    <li className='px-2'>
+                <ul className='flex py-5 justify-between items-center'>
+                    <li className='px-20'>
                         <BsFillMoonStarsFill onClick={handleChange} className='cursor-pointer text-2xl dark:text-white' />
                     </li>
+                    <li className='px-20'>
+                        <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8'
+                            href={cv}
+                            download='CV.pdf'>
+                            Resume
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <nav className='flex p-5 justify-center'>
+                <ul className='flex py-5 items-center'>
                     <li className='px-2'>
                         <Link className=' dark:text-white' to='/portfolio'>HOME</Link>
                     </li>
@@ -27,13 +38,6 @@ export default function BannerHeader({ darkMode: darkModeState, updateDarkModeSt
                     <li className='px-2'>
                         <Link className=' dark:text-white' to='/projects'>PROJECTS</Link>
                     </li>
-                    <div>
-                        <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8'
-                            href={cv}
-                            download='CV.pdf'>
-                            Resume
-                        </a>
-                    </div>
                 </ul>
             </nav>
         </section>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,9 @@
 import devicon from '../images/decoderdude.png';
 
 export default function Home() {
-
     return (
         <section className='min-h-screen'>
-            <div className='text-center p-10'>
+            <div className='text-center'>
                 <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl dark:text-white'>Dan Hernandez</h2>
                 <h3 className='text-2xl py-2 md:text-3xl dark:text-white'>Full Stack Developer</h3>
                 <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-white'>
